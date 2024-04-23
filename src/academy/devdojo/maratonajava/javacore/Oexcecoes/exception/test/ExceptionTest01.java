f9b3bdd94a41e0d504cc31544193529d2e453242
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-04-23 18:39:28 -0300

    exceções checked

--- src/academy/devdojo/maratonajava/javacore/Oexcecoes/exception/test/ExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexcecoes/exception/test/ExceptionTest01.java
@@ -4,17 +4,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExceptionTest01 {
-    public static void main(String[] args) {
-        criarNovoArquivo();
+    public static void main(String[] args) throws IOException {
+        ExceptionTest01.criarNovoArquivo();
     }
 
-    private static void criarNovoArquivo() {
+    public static void criarNovoArquivo() throws IOException {
         File file = new File("arquivo\\teste.txt");
         try {
             boolean isCriado = file.createNewFile();
             System.out.println("Arquivo criado " + isCriado);
         }catch (IOException exception) {
             exception.printStackTrace();
+            throw exception;
         }
     }
 }

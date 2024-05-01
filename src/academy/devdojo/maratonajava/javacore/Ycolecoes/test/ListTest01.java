811bbf6919c9fb435f1a1d622bb4b2caf471d563
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-05-01 17:18:31 -0300

    list2

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,12 +6,22 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(); //1.5
+        List<String> nomes2 = new ArrayList<>();
         nomes.add("Kenji");
         nomes.add("Breno");
+        nomes2.add("Carlos");
+        nomes2.add("Henrique");
 //        nomes.add(123); //erro pois é uma lista de String
+        nomes.remove(1);//removendo pelo índice
+        nomes.remove("Breno"); //removendo pelo objeto
 
+        nomes.addAll(nomes2);
         for (String nome: nomes) {
             System.out.println(nome);
         }
+
+        /////////////////////////////////////////////
+        List<Integer> inteiros = new ArrayList<>();
+        inteiros.add(1);//umboxing e autoboxing
     }
 }

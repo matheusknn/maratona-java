a08ed02b6b58fef4a3d80d2c49d7b8a789041f17
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-05-05 05:14:31 -0300

    classes anônimas

--- src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/AnonymousClassesTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/AnonymousClassesTest01.java
@@ -1,7 +1,27 @@
 package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
 
+class Animal {
+    public void walk() {
+        System.out.println("Animal walking");
+    }
+}
+
+//class Cachorro extends Animal {
+//    @Override
+//    public void walk() {
+//        System.out.println("Cachorro walking");
+//    }
+//}
 public class AnonymousClassesTest01 {
     public static void main(String[] args) {
+        Animal animal = new Animal() { //criando uma subclasse de animal sem nome que sobrescreve o método walk
 
+            @Override
+            public void walk() {
+                System.out.println("walking in the shadow");
+            }
+        };
+
+        animal.walk();
     }
 }
